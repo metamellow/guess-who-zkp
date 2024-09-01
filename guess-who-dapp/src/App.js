2a2a2bff426924a696ccc34de421beb01afd8e3b
metamellow <115264178+metamellow@users.noreply.github.com>
2024-09-01 13:10:03 +0700

    Revert App.js to original state 2

--- guess-who-dapp/src/App.js
+++ guess-who-dapp/src/App.js
@@ -2,14 +2,15 @@
 import React, { useMemo } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { WalletProvider } from '@demox-labs/aleo-wallet-adapter-react';
-import { WalletModalProvider, WalletMultiButton } from '@demox-labs/aleo-wallet-adapter-reactui';
+import { WalletModalProvider } from '@demox-labs/aleo-wallet-adapter-reactui';
 import { LeoWalletAdapter } from '@demox-labs/aleo-wallet-adapter-leo';
 
 import Home from './components/Home';
 import CreateGame from './components/CreateGame';
 import JoinGame from './components/JoinGame';
 import GameBoard from './components/GameBoard';
-import ErrorBoundary from './components/ErrorBoundary';
+import WalletConnectionButton from './components/WalletConnectionButton';
+import PlayerBalance from './components/PlayerBalance';
 
 import '@demox-labs/aleo-wallet-adapter-reactui/styles.css';
 import './styles/App.css';
@@ -25,6 +26,7 @@ function App() {
   );
 
   return (
+<<<<<<< HEAD
     <ErrorBoundary>
       <WalletProvider
         wallets={wallets}
@@ -45,6 +47,24 @@ function App() {
         </WalletModalProvider>
       </WalletProvider>
     </ErrorBoundary>
+=======
+    <WalletProvider wallets={wallets} autoConnect>
+      <WalletModalProvider>
+        <Router>
+          <div className="App">
+            <WalletConnectionButton />
+            <PlayerBalance />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/create" element={<CreateGame />} />
+              <Route path="/join" element={<JoinGame />} />
+              <Route path="/game/:id" element={<GameBoard />} />
+            </Routes>
+          </div>
+        </Router>
+      </WalletModalProvider>
+    </WalletProvider>
+>>>>>>> parent of 244201a (wallet connect streamlined)
   );
 }
 
